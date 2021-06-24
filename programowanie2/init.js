4619be60e929da29cd56cbdb2938192a4128be55
Havystar <31964929+Havystar@users.noreply.github.com>
2021-06-24 13:59:33 +0200

    model, load model in init

--- programowanie2/init.js
+++ programowanie2/init.js
@@ -1,2 +1,10 @@
+model = null;
+const MODELPATH = "/src/model.json";
+tf.loadLayersModel(MODELPATH)
+  .then(function (model_) {
+    model = model_;
+  })
+  .catch((error) => console.log(error));
+
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
